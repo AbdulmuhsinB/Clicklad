fa37a279973b9842e165d2c07ede3796463a9209
Abdulmuhsin Baksh <muhsin.baksh04@gmail.com>
2024-07-22 15:51:22 -0400

    updated and made the navbar more responsive

--- clicklad-app/components/Navbar.js
+++ clicklad-app/components/Navbar.js
@@ -1,14 +1,21 @@
-import React from 'react';
+'use client';
+import React, { useState } from 'react';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
       <div className={styles.leftSection}>
         <a href="/" className={styles.logoLink}>
           <img src="/logo.png" alt="Logo" className={styles.navbarLogo} />
         </a>
-        <div className={styles.navbarLinks}>
+        <div className={`${styles.navbarLinks} ${isOpen ? styles.active : ''}`}>
           <a href="/about-us">About Us</a>
           <a href="/product">Products</a>
           <a href="/location">Locations</a>
@@ -20,6 +27,23 @@ export default function Navbar() {
         Cart
         <img src="/icons/cart-icon.png" alt="Cart" className={styles.cartIcon} />
       </a>
+      <img 
+        src="/icons/hamburger-icon.png" 
+        alt="Hamburger" 
+        className={styles.hamburgerIcon} 
+        onClick={toggleMenu} 
+      />
+      {isOpen && (
+        <div className={styles.overlay} onClick={toggleMenu}>
+          <div className={styles.popup}>
+            <a href="/about-us">About Us</a>
+            <a href="/product">Products</a>
+            <a href="/location">Locations</a>
+            <a href="/FAQ">FAQs</a>
+            <a href="/contact">Contact</a>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
